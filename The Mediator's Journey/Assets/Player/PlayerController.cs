dbf1df13c1f774b31105aa39121b3a1c1664d348
Tomas Daniel Nieto <tomasdaniel09@hotmail.com>
2025-10-26 17:12:13 -0400

    Add player jump animation

--- The Mediator's Journey/Assets/Player/PlayerController.cs
+++ The Mediator's Journey/Assets/Player/PlayerController.cs
@@ -9,14 +9,12 @@ public class PlayerController : MonoBehaviour
     private Animator animator;
 
     [SerializeField] private float movementSpeed = 5f;
-    [SerializeField] private float jumpPower = 10f;
+    [SerializeField] private float jumpPower = 6f;
 
     private float horizontalInput;
     private bool isFacingRight = true;
     private bool jumpRequested = false;
 
-
-    // Start is called before the first frame update
     void Start()
     {
         rigidBody2D = GetComponent<Rigidbody2D>();
@@ -25,6 +23,7 @@ public class PlayerController : MonoBehaviour
 
     void Update()
     {
+        // Handle facing flip based on input while on ground or in air movement direction
         if (horizontalInput > 0 && !isFacingRight)
         {
             Flip();
@@ -34,16 +33,13 @@ public class PlayerController : MonoBehaviour
             Flip();
         }
 
-        if (horizontalInput != 0)
-        {
-            animator.SetBool("isWalking", true);
-        }
-        else
-        {
-            animator.SetBool("isWalking", false);
-        }
+        // Walking animation
+        animator.SetBool("isWalking", horizontalInput != 0 && IsGrounded());
+
+        // Jump/fall animator control
+        UpdateJumpAnimatorFlags();
     }
-    
+
     void FixedUpdate()
     {
         Vector2 currentVelocity = rigidBody2D.velocity;
@@ -51,7 +47,20 @@ public class PlayerController : MonoBehaviour
         float timeScale = TimeController.Instance.TimeScale;
         currentVelocity.x = horizontalInput * movementSpeed * timeScale;
 
-        if (jumpRequested && IsGrounded()) {
+        if (jumpRequested && IsGrounded())
+        {
+            // If starting jump from standstill, prefer forward jump sprite
+            if (Mathf.Approximately(horizontalInput, 0f))
+            {
+                animator.SetBool("isJumpingForward", true);
+                animator.SetBool("isJumpingRight", false);
+            }
+            else
+            {
+                animator.SetBool("isJumpingRight", true);
+                animator.SetBool("isJumpingForward", false);
+            }
+
             currentVelocity.y = jumpPower;
             jumpRequested = false;
         }
@@ -59,11 +68,23 @@ public class PlayerController : MonoBehaviour
         rigidBody2D.velocity = currentVelocity;
     }
 
-
     public void Move(InputAction.CallbackContext context)
     {
         horizontalInput = context.ReadValue<Vector2>().x;
 
+        // If in air and player moves, switch to side jump sprite and ensure orientation matches movement
+        if (!IsGrounded())
+        {
+            if (!Mathf.Approximately(horizontalInput, 0f))
+            {
+                animator.SetBool("isJumpingRight", true);
+                animator.SetBool("isJumpingForward", false);
+
+                // Flip to match movement direction immediately
+                if (horizontalInput > 0 && !isFacingRight) Flip();
+                if (horizontalInput < 0 && isFacingRight) Flip();
+            }
+        }
     }
 
     public void Jump(InputAction.CallbackContext context)
@@ -71,6 +92,31 @@ public class PlayerController : MonoBehaviour
         if (context.performed && IsGrounded())
         {
             jumpRequested = true;
+        }
+    }
+
+    private void UpdateJumpAnimatorFlags()
+    {
+        bool grounded = IsGrounded();
+
+        if (grounded)
+        {
+            // On ground, clear jump flags
+            animator.SetBool("isJumpingForward", false);
+            animator.SetBool("isJumpingRight", false);
+            return;
+        }
+
+        // In air: decide which jump sprite to show
+        if (!Mathf.Approximately(horizontalInput, 0f))
+        {
+            animator.SetBool("isJumpingRight", true);
+            animator.SetBool("isJumpingForward", false);
+        }
+        else
+        {
+            animator.SetBool("isJumpingForward", true);
+            animator.SetBool("isJumpingRight", false);
         }
     }
 
