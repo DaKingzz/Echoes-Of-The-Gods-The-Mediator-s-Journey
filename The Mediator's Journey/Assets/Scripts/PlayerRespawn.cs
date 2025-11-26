a7e732293ed4ca5c5b33e86769c83c3480ac47b4
Arturo <DaKingzz@users.noreply.github.com>
2025-11-25 20:46:57 -0500

    Revert "Merge pull request #39 from DaKingzz/revert-38-ikarusRoad-design"
    This reverts commit b352e2893baa716f96d7a2425f50d1c50375a672, reversing
    changes made to b8d74015fd810e83def9a54752d4a1602df70c78.

--- The Mediator's Journey/Assets/Scripts/PlayerRespawn.cs
+++
@@ -1,9 +0,0 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
-
-public class PlayerRespawn : MonoBehaviour
-{
-    [SerializeField] private PlayerController player;
-
-}
